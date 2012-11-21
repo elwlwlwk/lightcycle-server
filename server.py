804ca93824c44efab61bdfab730d8c18446d2fbd
Jinkyu Yi <jincreator@jincreator.net>
2012-11-21 16:19:07 +0900

    Clean up code and fix dangerous data parsing.

--- server.py
+++ server.py
@@ -4,26 +4,27 @@ import socket
 
 class Player:
 	def __init__(self, socket, ID):
-		self.socket= socket
-		self.ID= ID	
+		self.socket = socket
+		self.ID = ID
 
 class EchoHandler(asyncore.dispatcher_with_send):
-
 	def handle_read(self):
 		data = self.recv(1024)
 		data = str(data).strip()
-		print("accept from clint")
+		print('accept from clint')
 		print('message is '+data)
-		if 'HELO' in data:
-			piece= data.split(' ')
-			player= Player(self ,piece[1])
-			players.append(player)
-		elif 'FUCKYOU' in data:
+		header = data.split()[0]
+		body = data.split()[1]
+		if 'HELO' == header:
+			players.append(Player(self, body))
+		elif 'FUCKYOU' == header:
 			for each in players:
 				if each.socket is not self:
 					each.socket.send(data.encode('utf-8'))
-class EchoServer(asyncore.dispatcher):
+		else:
+			print('Unknown message! The header is '+header)
 
+class EchoServer(asyncore.dispatcher):
 	def __init__(self, host, port):
 		asyncore.dispatcher.__init__(self)
 		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,7 +35,7 @@ class EchoServer(asyncore.dispatcher):
 	def handle_accepted(self, sock, addr):
 		handler = EchoHandler(sock)
 
-if __name__== "__main__":
-	players=[]
+if __name__ == "__main__":
+	players = []
 	server = EchoServer('0.0.0.0', 9999)
 	asyncore.loop()
