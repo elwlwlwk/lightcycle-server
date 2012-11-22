b75888d96de171c6052bb3c026f03d666df09535
Jinkyu Yi <jincreator@jincreator.net>
2012-11-22 11:14:14 +0900

    Clean up code - also fix decode packet, space in body, threading in client.

--- server.py
+++ server.py
@@ -3,16 +3,16 @@ import asyncore
 import socket
 import sqlite3
 class gamedb:
-	def __init__(self, name='lcdb'):
-		self.conn=sqlite3.connect(name)
-		c=self.conn.cursor()
+	def __init__(self, name = 'lcdb'):
+		self.conn = sqlite3.connect(name)
+		c = self.conn.cursor()
 		try:
 			c.execute('create table game (data text)')
 		except sqlite3.OperationalError:
 			print('Table already exist.')
 
 	def add(self, data):
-		c=self.conn.cursor()
+		c = self.conn.cursor()
 		c.execute('insert into game values (?)',(data,))
 		self.conn.commit()
 		c.close
@@ -21,45 +21,45 @@ class Player:
 	def __init__(self, socket, ID):
 		self.socket = socket
 		self.ID = ID
-		self.ready= 0
+		self.ready = 0
 
 class EchoHandler(asyncore.dispatcher_with_send):
 	def handle_read(self):
 		data = self.recv(1024)
-		data = str(data).strip();
+		data = data.decode().strip();
 		print('accept from clint')
 		print('message is '+data)
-		splited= data.split()
+		splited = data.split()
 		header = splited[0]
 		if len(splited) > 1:
-			body = splited[1]
+			body = data[len(header):]
 			db.add(body) #TODO: Use db for specific data.
-		if 'HELO' in header:
+		if 'HELO' == header:
 			players.append(Player(self, body))
-		elif 'MGHG' in header:
+		elif 'MGHG' == header:
 			for each in players:
-#				if each.socket is not self:
-				each.socket.send(data.encode('utf-8')+' '.encode('utf-8')+str(each.ID).encode('utf-8'))
-		elif 'READY' in header:
+				if each.socket is not self:
+					msg = data+' '+str(each.ID)
+					each.socket.sendall(msg.encode())
+		elif 'READY' == header:
 			for each in players:
 				if each.socket is self:
-					print('set readey '+each.ID)
-					each.ready= 1
-			AllReadyed= 1
+					print('set ready '+each.ID)
+					each.ready = 1
+			allready = 1
 			for each in players:
 				print(each.ID+' is '+str(each.ready))
-				AllReadyed= AllReadyed* each.ready
-			if AllReadyed is 1:
+				allready *= each.ready
+			if allready is 1:
 				print('all clients are ready')
 				for each in players:
 					print('send start message')
-					SMsg= 'START'.encode('UTF-8')
-					each.socket.send(SMsg.strip())
-				
+					startmsg = 'START'.encode()
+					each.socket.sendall(startmsg.strip())
 		else:
 			print('Unknown message! The header is '+header)
 			for each in players:
-				each.socket.send(data.encode('utf-8'))
+				each.socket.sendall(data.encode())
 
 class EchoServer(asyncore.dispatcher):
 	def __init__(self, host, port):
@@ -73,7 +73,7 @@ class EchoServer(asyncore.dispatcher):
 		handler = EchoHandler(sock)
 
 if __name__ == "__main__":
-	db=gamedb()
+	db = gamedb()
 	players = []
 	server = EchoServer('0.0.0.0', 9999)
 	asyncore.loop()
